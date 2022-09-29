ec428b399a32defc6fcda9fb91efdcb1704e72c6
bwx1y <nedynugroho2007@gmail.com>
2022-09-29 23:42:14 +0700

    update

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,12 +1,13 @@
 window.onload = () => {
   $.getJSON("https://script.google.com/macros/s/AKfycbxekf4npZOTDMwYJgTqsBYkgsn45lYHY97x8fagunJ4sIrdam_qsdXy9reCDsEDTWzM/exec?action=getData", (res) => {
     $.each(res, (i, data) => {
-      $("#Data").append(`<div class="card border-info mb-3 text-center mt-3">
+      $("#Data").append(`<div class="col-md-4">
+      <div class="card border-info mb-3 text-center mt-3">
     <div class="card-header bg-transparent borderdark">" 🙂 "</div>
     <div class="card-body" >
       <h5 class="card-text">${data.message}</h5>
     </div>
-    </div>`);
+    </div></div>`);
       if (data.person) {
         $(".card-body").append(`<div class="card-footer bg-transparent border-">${data.person} : <p>" ${data.reply}"</p></div>`);
       }
